e87b090b1d865a88269e9726b02b110f157dfda4
Jamie H Pham <jpham1109@gmail.com>
2021-04-30 18:23:17 -0400

    Done but the tests errored

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,13 @@
-// write your CatList component here
+
+const CatList = ({catPics=[]}) => {
+    const catImages = catPics.map(cat => 
+        <img key={cat.id} src={cat.source_url} alt="cat"/>
+        )
+    return (
+        <div>
+            {catImages}
+        </div>
+    )
+}
+
+export default CatList
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import CatList from "./CatList.js"
+import { fetchCats } from "./catsSlice.js";
 
 function Cats() {
+  const catPics = useSelector(state => state.entities)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchCats())
+  }, [dispatch])
+ 
   return (
-    <div>
+    <div className="App">
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      <CatList catPics={catPics}/>
     </div>
   );
 }
